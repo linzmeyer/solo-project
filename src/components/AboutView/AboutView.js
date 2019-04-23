1d375c72fc527d93462406c6f62fb81ec15a677b
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-23 13:26:45 -0500

    added Fugaz One font

--- src/components/AboutView/AboutView.js
+++ src/components/AboutView/AboutView.js
@@ -5,10 +5,20 @@ import Header from '../Header/Header';
 class AboutView extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <h1>Hello AboutView</h1>
-        <p>This about page is for anyone to read!</p>
+      <div className="AboutView-wrapper">
+        <Header header="About" />
+        <div>
+          <h2>Your Mission</h2>
+          <p>Incididunt fugiat ad est qui aliqua non eu qui et ut et laboris. Ullamco sit exercitation cupidatat enim commodo. Adipisicing eiusmod laboris dolor proident labore sint proident duis sint ad aliquip amet.</p>
+        </div>
+        <div>
+          <h2>Your Reward</h2>
+          <p>Aliqua excepteur duis labore exercitation commodo voluptate ipsum excepteur elit qui sit. Laborum nisi ut nisi ullamco exercitation id laborum aute do consequat. Sunt minim reprehenderit sunt aute commodo aute. Officia sunt qui anim Lorem duis pariatur dolor minim mollit laboris consectetur consequat.</p>
+        </div>
+        <div>
+          <h2>About This App</h2>
+          <p>Anim nulla cillum id proident proident sint voluptate do nisi in culpa et do ad. Amet voluptate nisi deserunt proident pariatur. In mollit magna qui fugiat eu. Sit fugiat labore aliqua irure tempor. Excepteur sunt enim nisi adipisicing. Esse minim laborum deserunt nostrud excepteur ipsum reprehenderit officia laboris laboris deserunt.</p>
+        </div>
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-
+import './Header.css';
 
 // The header that renders should be propped from the parent that is using this 
 // this component.
 class Header extends Component {
   render() {
     return (
-      <h1>{this.props.header}</h1>
+      <h1 className="Header-wrapper ">{this.props.header}</h1>
     );
   }
 }
