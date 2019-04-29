4d0cef8b728e810a3801534f7e364f9009a7eae3
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-29 15:40:47 -0500

    score transactions created

--- server/routes/user-clue-score.router.js
+++ server/routes/user-clue-score.router.js
@@ -2,16 +2,57 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// // GET all clues from the database
-// router.get('/', (req, res) => {
-//   const queryText = `SELECT "clue_number", "description", "answer", "location" FROM "clues";`;
-//   pool.query(queryText)
-//     .then((result) => { res.send(result.rows); })
-//     .catch((err) => {
-//       console.log('Error completing SELECT all clues query', err);
-//       res.sendStatus(500);
-//     });
-// }); // End GET
+// update the user's clue score
+router.put('/', async (req, res) => {
+  let userId = req.body.userId;
+  let newScore = req.body.newScore;
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+
+    // text to be inserted into user table for specific user based on id
+    let sqlText = `UPDATE "user" SET "clue_score" = ($1) WHERE "id" = ($2);`;
+    await client.query( sqlText, [newScore, userId] );
+
+    await client.query( 'COMMIT' );
+    res.sendStatus( 200 );
+  } catch (err) {
+    await client.query( 'ROLLBACK' );
+    console.log( `Error with UPDATE`, err );
+    res.sendStatus( 500 );
+  } finally {
+    client.release()
+  }
+}); // END PUT TRANSACTION
+
+// get the user's clue score
+router.get('/', async (req, res) => {
+  console.log('get req.user.id:', req.user.id)
+  let userId = req.user.id;
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    // sql text to select the clue score of a user based on their id
+    let sqlText = `SELECT "clue_score" FROM "user" WHERE "id" = $1;`;
+    await client.query( sqlText, [userId])
+      .then((result) => { 
+        // get clue_score by itself in an object
+        let clueScore = result.rows[0];
+        res.send( clueScore );
+      })
+      .catch((err) => {
+        console.log('Error with SELECT', err);
+        res.sendStatus(500);
+      });
+    await client.query( 'COMMIT' )
+  } catch (err) {
+    await client.query( 'ROLLBACK' )
+    console.log(`Error with try: SELECT`, err);
+    res.sendStatus( 500 );
+  } finally {
+    client.release()
+  }
+}); // END GET TRANSACTION
 
 
 module.exports = router;
