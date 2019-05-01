36d809f0b1fd2b8f484ea4826c0625c2e6f743dc
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-01 15:47:20 -0500

    <hr> moved

--- src/components/ClueListItem/ClueListItem.js
+++ src/components/ClueListItem/ClueListItem.js
@@ -5,13 +5,13 @@ class ClueListItem extends Component {
   render () {
     return (
       <div>
+        <hr></hr>
         <h3>Clue {this.props.clue.clue_number}</h3>
         <p>{this.props.clue.description}</p>
         <p>{this.props.clue.answer}</p>
         
         <h4>Location:</h4>
         <p>{this.props.clue.location}</p>
-        <hr></hr>
       </div>
 		);
   }
