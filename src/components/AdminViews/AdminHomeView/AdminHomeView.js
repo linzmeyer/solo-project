17b4a5ecfae60f6459d0c718b47b69ccf867c90d
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-04 10:39:44 -0500

    added classname to btns

--- src/components/AdminViews/AdminHomeView/AdminHomeView.js
+++ src/components/AdminViews/AdminHomeView/AdminHomeView.js
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../../Header/Header';
 import Navbar from '../../Navbar/Navbar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './AdminHomeView.css';
-
 
 class AdminHomeView extends Component {
   render() {
@@ -17,19 +15,19 @@ class AdminHomeView extends Component {
         <div className="admin-home-links" >
           <div>
             <Link to="/admin/edit-game" >
-              <button>Edit Discovery Game</button>
+              <button className="btn-home" >Edit Discovery Game</button>
             </Link>
           </div>
 
           <div>
             <Link to="/admin/game-details" className="nav-item" >
-              <button>Current Game Details</button>
+              <button className="btn-home">Current Game Details</button>
             </Link>
           </div>
 
           <div>
             <Link to="/admin/manage-users" className="nav-item" >
-              <button>Manage Users</button>
+              <button className="btn-home">Manage Users</button>
             </Link>
           </div>
         </div>
