602454502dbad9774f7c63b153366a9e423e6a67
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-04 10:48:12 -0500

    added icon and route when clicked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter } from 'react-router-dom';
 
 // The header that renders should be propped from the parent that is using this 
 // this component.
 class Header extends Component {
+
+  routeToClue1 = () => {
+    this.props.history.push('/clues/1');
+  }
+
   render() {
     return (
-      <h1 className="Header-wrapper"><FontAwesomeIcon icon={["fab", "earlybirds"]} /> {this.props.header}</h1>
+      <div className="Header-wrapper" >
+        <FontAwesomeIcon
+          icon={["fab", "earlybirds"]}
+          className="header-icon"
+          onClick={ this.routeToClue1 }
+        />
+        <h2 className="title-header">Discover Oakdale</h2>
+        <h1 className="view-header">{this.props.header}</h1>
+      </div>
     );
   }
 }
 
-export default Header;
+export default withRouter( Header );
