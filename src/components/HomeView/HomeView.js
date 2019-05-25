e9f3ceaa24ee3246a38314d95c0789a8e679d682
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-25 15:21:42 -0500

    Now conditionally renders based on admin status

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
+import AdminHomeView from '../AdminViews/AdminHomeView/AdminHomeView';
 
 class HomeView extends Component {
+  // if the user is an admin
+  renderForAdmin = () => {
+    if ( this.props.user.is_admin ) {
+      return (
+        <AdminHomeView />
+      );
+    }
+  }
+
+  // if the user is registerd & if the user isn't an admin
+  renderForPlayer = () => {
+    if ( this.props.user.id && !this.props.user.is_admin ) {
+      return (
+        <div>
+          <Header header="HOME" />
+          <Navbar currentView="HOME" />
+          <h1 id="welcome">Welcome, { this.props.user.username }!</h1>
+          <p>Use the links above to get started!</p>
+        </div>
+      );
+    }
+  }
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        <Header header="HOME" />
-        <Navbar currentView="HOME" />
-        <h1 id="welcome">Welcome, { this.props.user.username }!</h1>
-        <p>Use the links above to get started!</p>
-        
+        { this.renderForAdmin() }
+        { this.renderForPlayer() }
       </div>
     );
   }
 }
 
-// Instead of taking everything from state, we just want the user info.
-// if you wanted, you could write this code like this:
 const mapStateToProps = ({ user, loginMode }) => ({ user, loginMode });
+
+// Other ways to write mapStateToProps:
 // const mapStateToProps = reduxState => ({ user: reduxState.user, loginMode: reduxState.loginMode });
 // const mapStateToProps = reduxState => ({ reduxState });
 
